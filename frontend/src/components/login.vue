a02a12b24ef4bb37a2f66669298c4b387c7e0dea
Numa52 <77755196+Numa52@users.noreply.github.com>
2023-03-06 09:57:09 -0600

    finished login.vue (hopefully lol)

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -9,3 +9,31 @@
     </form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    async login() {
+      // make an API call to authenticate the user
+      try {
+        const response = await axios.post('/api/login', {
+          username: this.username,
+          password: this.password
+        })
+        // redirect the user to the dashboard page
+        this.$router.push('/dashboard')
+      } catch (error) {
+        // show an error message
+        console.error(error)
+        alert('Invalid username and/or password')
+      }
+    }
+  }
+}
+</script>
