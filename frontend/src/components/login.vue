45db936acdf630b84ec5404dc68b3349f8d58065
Numa52 <77755196+Numa52@users.noreply.github.com>
2023-03-06 09:41:30 -0600

    working on login page

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,11 @@
+// login.vue 
+
+<template>
+  <div>
+    <form @submit.prevent="login">
+      <input type="text" v-model="username" placeholder="Username">
+      <input type="password" v-model="password" placeholder="password">
+      <button type="submit">Login!</button>
+    </form>
+  </div>
+</template>
