93f06cb0d2e00b3697c9042bbd88c1bd96329c21
Numa52 <77755196+Numa52@users.noreply.github.com>
2023-03-08 23:45:46 -0600

    Login Form
    Updated login form to a more presentable form

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -1,14 +1,52 @@
 <template>
-  <div class="login-box">
-    <h1>Login Form</h1>
-    <form @submit.prevent="store.login(username, password)" novalidate="true">
-      <label for="username">Username:</label>
-      <input type="text" id="username" v-model="username" placeholder="Enter your username">
-      <label for="password">Password:</label>
-      <input type="password" id="password" v-model="password" placeholder="Enter your password">
-      <button type="submit">Login</button>
-    </form>
-  </div>
+  <main>
+    <div>
+      <h1
+        class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10"
+      >
+        Login
+      </h1>
+    </div>
+    <div class="px-10 py20">
+      <form @submit.prevent="store.login(username, password)" novalidate="true">
+        <!--grid container-->
+        <div
+          class="grid grid-cols-1 sm: grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10"
+        ></div>
+        <!--form field-->
+        <div class="flex flex-col">
+          <label class="block">
+            <span class="text-gray-700">Username</span>
+            <span style="color: #ff0000"></span>
+            <input
+              type="text"
+              id="username"
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="username"
+            >
+          </label>
+        </div>
+        <div class="flex flex-col">
+          <label class="block">
+            <span class="text-gray-700">Password</span>
+            <span style="color: #ff0000"></span>
+            <input
+              type="password"
+              id="password"
+              class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              v-model="password"
+            >
+          </label>
+
+          <div class="flex justify-between mt-10 mr-20">
+            <button class="bg-red-700 text-white rounded" type="submit">
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </main>
 </template>
 
 
