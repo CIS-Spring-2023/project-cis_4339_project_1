ab561a9368ed8d9d9972c00e56ba384657eae68a
Numa52 <77755196+Numa52@users.noreply.github.com>
2023-04-24 21:25:27 -0500

    model.js
    need to fix model.js to fix service data schema

--- backend/models/models.js
+++ backend/models/models.js
@@ -129,10 +129,24 @@ const eventDataSchema = new Schema(
   }
 )
 
+// add services data 
+const serviceDataSchema = new Schema(
+
+//fill data in 
+
+
+
+
+
+)
+
+
+
 // create models from mongoose schemas
 const clients = mongoose.model('client', clientDataSchema)
 const orgs = mongoose.model('org', orgDataSchema)
 const events = mongoose.model('event', eventDataSchema)
+const services = mongoose.model('service', serviceDataSchema)
 
 // package the models in an object to export
-module.exports = { clients, orgs, events }
+module.exports = { clients, orgs, events, services }
