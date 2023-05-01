3fe6fbd8590ef4e593c02d7e89540926efa9cfde
Numa52 <77755196+Numa52@users.noreply.github.com>
2023-04-30 19:57:55 -0500

    Update PieChart.js

--- frontend/src/assets/PieChart.js
+++ frontend/src/assets/PieChart.js
@@ -1,46 +1,6 @@
 import axios from "axios"; // Import Axios library
 
 export const PieChart = {
-<<<<<<< HEAD
-  type: 'pie',
-  data: {
-    labels: [
-      '77459',
-      '77477',
-      '75201',
-      '73344',
-      '75002',
-      '75011',
-      '75022',
-      '75014'
-    ],
-    datasets: [
-      {
-        label: 'Zip Codes',
-        data: [5, 8, 9, 2, 10, 14, 27, 14],
-        backgroundColor: [
-          '#c31a36',
-          '#d03132',
-          '#db452e',
-          '#e55729',
-          '#ee6a22',
-          '#f57c1b',
-          '#fb8e11',
-          '#ffa104'
-        ],
-        borderColor: 'black',
-        borderWidth: 2
-      }
-    ]
-  },
-  options: {
-    responsive: true,
-    lineTension: 1
-  }
-}
-
-export default PieChart
-=======
     type: "pie",
     data: {
       labels: [],
@@ -80,4 +40,3 @@ export default PieChart
   // });
 
   export default PieChart;
->>>>>>> 14396185133bc5e543415fcc7c5745d4f0dd4cea
